7316f138f5f3f8799df0ecca61ef1e62d5b7c18a
Manuel Weilharter <manuelweilharter@gmail.com>
2022-02-07 20:54:24 +0100

    add option to withdraw shitcoins

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -1,18 +1,17 @@
 module.exports = async (kraken, validate, { getEnv }) => {
-  const [maxFee, key] = getEnv('KRAKEN_MAX_REL_FEE', 'KRAKEN_WITHDRAW_KEY')
+  const [asset, maxFee, key] = getEnv('KRAKEN_API_CRYPTO', 'KRAKEN_MAX_REL_FEE', 'KRAKEN_WITHDRAW_KEY')
 
   // https://api.kraken.com/0/private/WithdrawInfo
-  const asset = 'XBT'
   const withdrawdetails = { asset, key, amount: 0 }
 
   // Get withdrawal information
   const { result: { limit, fee } } = await kraken.api('WithdrawInfo', withdrawdetails)
-  const relFee = 1/parseFloat(limit)*parseFloat(fee)
+  const relFee = 1 / parseFloat(limit) * parseFloat(fee)
 
-  console.log(`💡  Relative fee of withdrawal amount: ${(relFee*100).toFixed(2)}%`)
+  console.log(`💡  Relative fee of withdrawal amount: ${(relFee * 100).toFixed(2)}%`)
 
   // Place withdrawal when fee is low enough (relatively)
-  if (relFee < maxFee/100) {
+  if (relFee < maxFee / 100) {
     console.log(`💰  Withdraw ${limit} ${asset} now.`)
     const withdraw = { asset, key, amount: limit }
     if (!validate) {
